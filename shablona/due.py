69103353fc65a8efffbaa8651eb7e982ccced43e
Yaroslav Halchenko <debian@onerussian.com>
2016-04-19 16:20:00 -0400

    BF: Updated PEP8 compliant due.py

--- shablona/due.py
+++ shablona/due.py
@@ -1,12 +1,12 @@
-# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
-# ex: set sts=4 ts=4 sw=4 noet:
-# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+# emacs: at the end of the file
+# ex: set sts=4 ts=4 sw=4 et:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### #
 """
 
-Stub file for a guaranteed safe import of duecredit constructs:  if duecredit is
-not available.
+Stub file for a guaranteed safe import of duecredit constructs:  if duecredit
+is not available.
 
-To use it, just place it into your project codebase to be imported, e.g. copy as
+To use it, place it into your project codebase to be imported, e.g. copy as
 
     cp stub.py /path/tomodule/module/due.py
 
@@ -17,18 +17,15 @@ Then use in your code as
 
     from .due import due, Doi, BibTeX
 
+See  https://github.com/duecredit/duecredit/blob/master/README.md for examples.
 
-Examples
---------
-
-TODO
-
-
-License:
-Originally a part of the duecredit, which is distributed under BSD-2 license.
+Origin:     Originally a part of the duecredit
+Copyright:  2015-2016  DueCredit developers
+License:    BSD-2
 """
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
+
 
 class InactiveDueCreditCollector(object):
     """Just a stub at the Collector which would not do anything"""
@@ -39,22 +36,24 @@ class InactiveDueCreditCollector(object):
     def dcite(self, *args, **kwargs):
         """If I could cite I would"""
         def nondecorating_decorator(func):
-             return func
+            return func
         return nondecorating_decorator
 
-    cite = load = add =  _donothing
+    cite = load = add = _donothing
 
     def __repr__(self):
         return self.__class__.__name__ + '()'
+
 
 def _donothing_func(*args, **kwargs):
     """Perform no good and no bad"""
     pass
 
 try:
-    from duecredit import *
+    from duecredit import due, BibTeX, Doi, Url
     if 'due' in locals() and not hasattr(due, 'cite'):
-        raise RuntimeError("Imported due lacks .cite. DueCredit is now disabled")
+        raise RuntimeError(
+            "Imported due lacks .cite. DueCredit is now disabled")
 except Exception as e:
     if type(e).__name__ != 'ImportError':
         import logging
@@ -63,3 +62,11 @@ except Exception as e:
     # Initiate due stub
     due = InactiveDueCreditCollector()
     BibTeX = Doi = Url = _donothing_func
+
+# Emacs mode definitions
+# Local Variables:
+# mode: python
+# py-indent-offset: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# End:
