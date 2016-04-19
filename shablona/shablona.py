1c777842dafe7950b2a3630fedc6ce60eda4aac8
Yaroslav Halchenko <debian@onerussian.com>
2016-04-19 14:40:59 -0400

    ENH: extend duecredit's definition for shablona
    added description (so it lists nicely, otherwise how could we know what
    it is about?),
    tags to specify that this is the canonical reference (not just an implementation
    of what is cited in the url)
    and 'path' since that one is not deduced automagically
    
    removed unused import to please flake8

--- shablona/shablona.py
+++ shablona/shablona.py
@@ -2,15 +2,19 @@ import numpy as np
 import pandas as pd
 import scipy.optimize as opt
 from scipy.special import erf
-from .due import due, Doi, BibTeX
+from .due import due, Doi
 
 __all__ = ["Model", "Fit", "opt_err_func", "transform_data", "cumgauss"]
 
 
 # Use duecredit (duecredit.org) to provide a citation to relevant work to
-# be cited. This does nothing, unless the user has duecredit installted,
+# be cited. This does nothing, unless the user has duecredit installed,
 # And calls this with duecredit (as in `python -m duecredit script.py`):
-due.cite(Doi("10.1167/13.9.30"))
+due.cite(Doi("10.1167/13.9.30"),
+         description="Template project for small scientific Python projects",
+         tags=["reference-implementation"],
+         path='shablona')
+
 
 def transform_data(data):
     """
