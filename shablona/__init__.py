312d1c8adf57c93a5fcd9156ac8da740f4d0aaac
arokem <arokem@gmail.com>
2016-06-15 17:02:06 -0700

    Import things from the future.

--- shablona/__init__.py
+++ shablona/__init__.py
@@ -1,2 +1,3 @@
+from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
 from .shablona import *  # noqa
--- shablona/shablona.py
+++ shablona/shablona.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, division, print_function
 import numpy as np
 import pandas as pd
 import scipy.optimize as opt
