5e1debfe131bbee8b49c26e4db3cd95553f5ca7e
Thor Kell <thorkell@spotify.com>
2018-09-17 14:35:01 -0400

    Check exit codes

--- scripts/test_tutorial_install.sh
+++ scripts/test_tutorial_install.sh
@@ -2,19 +2,65 @@
 
 echo "Checking git and conda"
 git --version
+if [ $? -ne 0 ]
+then
+    echo "Error!  Git is not installed!"
+fi
+
 conda --version
-echo "\n"
+if [ $? -ne 0 ]
+then
+    echo "Error!  Anaconda is not installed!"
+fi
 
-echo "Checking python packages and python version"
-python -c "import flake8"
-python -c "import jupyter"
-python -c "import matplotlib"
-python -c "import numpy"
-python -c "import pandas"
-python -c "import requests"
-python -c "import scipy"
-python -c "import sklearn"
-python -c "import sphinx"
+echo "Checking Python version"
 python --version
+if [ $? -ne 0 ]
+then
+    echo "Error!  Python is not installed, which probably means that Anaconda did not install correctly!"
+fi
 
-echo "Success!"
+echo "Checking Python packages ..."
+python -c "import flake8"
+if [ $? -ne 0 ]
+then
+    echo "Error!  You're missing flake8, a Python package."
+fi
+
+python -c "import jupyter"
+if [ $? -ne 0 ]
+then
+    echo "Error!  You're missing jupyter, a Python package."
+fi
+
+python -c "import matplotlib"
+if [ $? -ne 0 ]
+then
+    echo "Error!  You're missing matplotlib, a Python package."
+fi
+
+python -c "import numpy"
+if [ $? -ne 0 ]
+then
+    echo "Error!  You're missing numpy, a Python package."
+fi
+
+python -c "import scipy"
+if [ $? -ne 0 ]
+then
+    echo "Error!  You're missing scipy, a Python package."
+fi
+
+python -c "import sklearn"
+if [ $? -ne 0 ]
+then
+    echo "Error!  You're missing sklearn, a Python package."
+fi
+
+python -c "import sphinx"
+if [ $? -ne 0 ]
+then
+    echo "Error!  You're missing sphinx, a Python package."
+fi
+
+echo "Success! Everything appears to be installed!"
