e484f368829b3a8e23ea8f346dba197b44688d91
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-08-21 09:21:57 -0400

    ENH: Add python_requires metadata to package

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
@@ -6,6 +7,13 @@ PACKAGES = find_packages()
 ver_file = os.path.join('shablona', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
+
+# Give setuptools a hint to complain if it's too old a version
+# 24.2.0 added the python_requires option
+# Should match pyproject.toml
+SETUP_REQUIRES = ['setuptools >= 24.2.0']
+# This enables setuptools to install wheel on-the-fly
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
 opts = dict(name=NAME,
             maintainer=MAINTAINER,
@@ -23,6 +31,8 @@ opts = dict(name=NAME,
             packages=PACKAGES,
             package_data=PACKAGE_DATA,
             install_requires=REQUIRES,
+            python_requires=PYTHON_REQUIRES,
+            setup_requires=SETUP_REQUIRES,
             requires=REQUIRES)
 
 
